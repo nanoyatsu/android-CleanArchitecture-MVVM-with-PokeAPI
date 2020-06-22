dcd69bfe2bbc819cec40fbeea6586c9dacfc3a5c
nanoyatsu <34255833+nredjap@users.noreply.github.com>
2020-06-23 00:39:26 +0900

    add Room database,dao (forgotten commit)

--- app/src/main/java/com/nanoyatsu/example/clean/poke/data/database/dao/PokeDao.kt
+++ app/src/main/java/com/nanoyatsu/example/clean/poke/data/database/dao/PokeDao.kt
@@ -1,15 +1,15 @@
 package com.nanoyatsu.example.clean.poke.data.database.dao
 
 import androidx.room.*
-import com.nanoyatsu.example.clean.poke.data.database.relation.PokeWithTypeAndAbility
+import com.nanoyatsu.example.clean.poke.data.database.relation.PokeCacheWithTypeAndAbility
 
 @Dao
 interface PokeDao {
     @Transaction
     @Query("Select * FROM poke_cache WHERE id == :id")
-    suspend fun getPoke(id: Int): PokeWithTypeAndAbility
+    suspend fun getPoke(id: Int): PokeCacheWithTypeAndAbility?
 
     @Transaction
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(row: PokeWithTypeAndAbility)
+    suspend fun insert(row: PokeCacheWithTypeAndAbility)
 }

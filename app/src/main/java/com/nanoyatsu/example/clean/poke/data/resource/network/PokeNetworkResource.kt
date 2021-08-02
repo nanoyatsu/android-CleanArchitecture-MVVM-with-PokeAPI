77f338ea89429be863cbdecde9436fba0750e5bb
nanoyatsu <34255833+nredjap@users.noreply.github.com>
2021-08-02 13:58:27 +0900

    migrating graphql-pokemon -> PokeAPI_GraphQL , improve FetchPokedex.graphql

--- app/src/main/java/com/nanoyatsu/example/clean/poke/data/resource/network/PokeNetworkResource.kt
+++ app/src/main/java/com/nanoyatsu/example/clean/poke/data/resource/network/PokeNetworkResource.kt
@@ -20,15 +20,6 @@ class PokeNetworkResource(private val client: ApolloClient) {
     }
     // fun get(id: Int) = client.getPokemon(id)
 
-    /**
-     * `pokemons` queryが、 `first` として整数を１つだけ受け付ける。
-     * 1だとNo.1のみ1件、151だとNo.1~151の151件返す。
-     */
-    suspend fun list(count: Int = FETCH_POKEMONS_COUNT): Response<FetchPokemonsQuery.Data> {
-        val query = FetchPokemonsQuery(count)
-        return client.query(query).toDeferred().await()
-    }
-
     suspend fun list(offset: Int, limit: Int): Response<FetchPokedexQuery.Data> {
         val query = FetchPokedexQuery(offset, limit)
         return client.query(query).toDeferred().await()
